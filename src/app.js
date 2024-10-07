0de92922107127f6f6c1209af36edf1665539530
Orangel Hernandez <orangel.hernandez1999@gmail.com>
2024-10-07 19:42:20 +0000

    reasignando ubicacion  del codigo JavaScript

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombre = ["el", "la", "los", "elle"];
+  let adjetivo = ["gran", "grande", "fino", "chico"];
+  let sustantivo = ["lacasa", "aluminio", "barcos", "canarios"];
+  let extencion = [".com", ".net", ".es", ".eu"];
+
+  for (let i = 0; i < pronombre.length; i++) {
+    for (let j = 0; j < adjetivo.length; j++) {
+      for (let k = 0; k < sustantivo.length; k++) {
+        for (let l = 0; l < extencion.length; l++) {
+          console.log(
+            `${pronombre[i]}${adjetivo[j]}${sustantivo[k]}${extencion[l]}`
+          );
+        }
+      }
+    }
+  }
 };
